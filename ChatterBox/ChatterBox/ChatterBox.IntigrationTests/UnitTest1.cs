a22c112151d7e1d010ab9a3e3e38a2feb630ce3f
Sandeep singh <sandeep.sappal96@gmail.com>
2026-07-12 20:41:42 +0530

    Add SignalR chat, test projects, and solution updates
    Added SignalR ChatHub and client UI. Introduced ChatterBox.IntigrationTests and ChatterBox.UnitTests with xUnit setup. Updated .gitignore and ChatterBox.slnx for new projects. Registered SignalR in Program.cs and added Visual Studio solution files.

---
+++ ChatterBox/ChatterBox/ChatterBox.IntigrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatterBox.IntigrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- SignalRChatApp/Program.cs
+++ SignalRChatApp/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.UseStaticFiles();
 
 app.Run();
